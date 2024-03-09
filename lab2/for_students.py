974048e0197e7a25d6b132ac78c7e7dd753a101c
jerzyszyjut <jerzy.szyjut@outlook.com>
2024-03-09 15:18:51 +0100

    Lab 2 initial solution

---
+++ lab2/for_students.py
@@ -0,0 +1,24 @@
+from itertools import compress
+import random
+
+
+def initial_population(individual_size, population_size):
+    return [[random.choice([True, False]) for _ in range(individual_size)] for _ in range(population_size)]
+
+
+def fitness(items, knapsack_max_capacity, individual):
+    total_weight = sum(compress(items['Weight'], individual))
+    if total_weight > knapsack_max_capacity:
+        return 0
+    return sum(compress(items['Value'], individual))
+
+
+def population_best(items, knapsack_max_capacity, population):
+    best_individual = None
+    best_individual_fitness = -1
+    for individual in population:
+        individual_fitness = fitness(items, knapsack_max_capacity, individual)
+        if individual_fitness > best_individual_fitness:
+            best_individual = individual
+            best_individual_fitness = individual_fitness
+    return best_individual, best_individual_fitness
